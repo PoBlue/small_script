4cbee7076e76ce14dd23e459315029864151596d
PoBlue <250299430@qq.com>
2017-11-16 22:18:43 +0800

    [csv] read csv and return rows in dict

--- csv-handler/utils.py
+++ csv-handler/utils.py
@@ -3,16 +3,33 @@ import csv
 headers = ['Symbol', 'Price', 'Date', 'Time', 'test']
 rows = [
     ('AA', 12, 'abc', '123', 'jj'),
-    ('AA', 12, 'abc', '123', 'jj'),
-    ('AA', 12, 'abc', '123', 'jj'),
-    ('AA', 12, 'abc', '123', 'jj'),
+    ('Ad', 12, 'abc', '123', 'jj'),
+    ('Ac', 12, 'abc', '123', 'jj'),
+    ('Ab', 12, 'abc', '123', 'jj'),
 ]
 path = 'example.csv'
 
+
 def export_csv(_headers, _rows, _path):
+    """
+        export_csv with headers rows and path
+    """
     with open(_path, 'w') as f:
         f_csv = csv.writer(f)
         f_csv.writerow(_headers)
         f_csv.writerows(_rows)
 
-export_csv(headers, rows, path)
+
+def read_csv(_path):
+    """
+        read_csv from _path
+    """
+    rows = []
+    with open(_path) as f:
+        f_csv = csv.DictReader(f)
+        for row in f_csv:
+            rows.append(row)
+    return rows
+
+# export_csv(headers, rows, path)
+# print(read_csv(path)[1]['Symbol'])
