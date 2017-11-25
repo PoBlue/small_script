65c83fdd61536d55ace3b1424079d4bf9df12d64
PoBlue <250299430@qq.com>
2017-11-25 20:41:43 +0800

    [tor] renew ip

--- tor-crawler/tor-pratice.py
+++ tor-crawler/tor-pratice.py
@@ -1,4 +1,12 @@
 import requests
+from stem import Signal
+from stem.control import Controller
+
+# signal TOR for a new connection 
+def renew_connection():
+    with Controller.from_port(port = 9051) as controller:
+        controller.authenticate(password="password")
+        controller.signal(Signal.NEWNYM)
 
 def get_tor_session():
     '''
