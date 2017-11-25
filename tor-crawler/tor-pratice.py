d52c0d6213bb6d61fce882ba2219ade62970f25a
PoBlue <250299430@qq.com>
2017-11-25 20:17:43 +0800

    [tor] get tor session

---
+++ tor-crawler/tor-pratice.py
@@ -0,0 +1,19 @@
+import requests
+
+def get_tor_session():
+    '''
+    get tor session with request
+    '''
+    session = requests.session()
+    session.proxies = {'http':  'socks5://127.0.0.1:9050',
+                       'https': 'socks5://127.0.0.1:9050'}
+    return session
+
+def main():
+    session = get_tor_session()
+    print(session.get("http://httpbin.org/ip").text)
+    print(requests.get("http://httpbin.org/ip").text)
+    pass
+
+if __name__ == '__main__':
+    main()
