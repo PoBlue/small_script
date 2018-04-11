777938e0c706b3287e3a5e6457f153c792873bd4
PoBlue <250299430@qq.com>
2018-04-11 22:10:18 +0800

    [alg] linked list info

---
+++ algorithm/README.md
@@ -0,0 +1,11 @@
+# 算法（红书）
+
+## Linked List
+特点
+- 有数据
+- 指向下一个元素
+
+用处：实现数据结构
+- bag
+- Queue (FIFO)
+- Stack (LIFO)
--- algorithm/linked_list.py
+++ algorithm/linked_list.py
@@ -3,7 +3,7 @@
 class Node:
     def __init__(self, val):
         self.val = val
-        self. next = None # the pointer initially points to nothing
+        self.next = None # the pointer initially points to nothing
 
     def traverse(self):
         node = self # start from the head node
